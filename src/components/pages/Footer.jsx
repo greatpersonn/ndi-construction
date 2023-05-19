6fb704d104232a7a823fc6969a85b86b81b28d8f
Valerij <44767739+greatpersonn@users.noreply.github.com>
2023-05-19 12:13:21 +0300

    upload updated files, settings, localization, create new component, create new logic and rework compoennts

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,9 +1,14 @@
+import Text from '../atoms/Text';
+import TextHeader from '../atoms/TextHeader';
+
 import './Footer.scss';
 
 const Footer = () => {
     return (
         <footer>
             <div className="container-footer">
+                <TextHeader header={'footer.title-company'} />
+                <Text contents={["footer.title-arhitecture"]} />
             </div>
         </footer>
     )
